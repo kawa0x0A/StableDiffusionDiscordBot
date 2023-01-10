128257ea32ec74bb6a765ecc0aa17c89ce76fdf2
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2023-01-10 22:47:23 +0900

    Discordアカウントでログインする機能を追加

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
 using StableDiffusionDiscordBot;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,19 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<DiscordAuthticateStateProvider>();
+
+builder.Services.AddScoped<AuthenticationStateProvider>(options => options.GetRequiredService<DiscordAuthticateStateProvider>());
+
+builder.Services.AddScoped<IAuthService, DiscordAuthService>();
+
+builder.Services.AddApiAuthorization();
+
+builder.Services.AddOptions();
+
+builder.Configuration.AddEnvironmentVariables().Build();
+
+Stripe.StripeConfiguration.ApiKey = builder.Configuration["StripeApiKey"];
 
 await builder.Build().RunAsync();
