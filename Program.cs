2bbf103a51807e0c05d8c5311374cc54a10314e4
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2023-01-11 13:30:03 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,11 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-#if DEBUG
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-#else
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://github.com/kawa0x0A/StableDiffusionDiscordBot/") });
-#endif
 
 builder.Services.AddScoped<DiscordAuthticateStateProvider>();
 
