8c0aa004e45b63b4ac558418c2e5bf7ef9f1681b
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2023-01-14 14:50:53 +0900

    Update Resources.Designer.cs

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -61,11 +61,56 @@ namespace StableDiffusionDiscordBot.Properties {
         }
         
         /// <summary>
+        ///   About Charges に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string AboutCharges {
+            get {
+                return ResourceManager.GetString("AboutCharges", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   What is this site? に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string AboutSite {
+            get {
+                return ResourceManager.GetString("AboutSite", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   A. Discord and credit card information cannot be verified directly by the site&apos;s creator. The site is also publicly available on &lt;a href=&quot;https://github.com/kawa0x0A/StableDiffusionDiscordBot&quot;&gt;Github&lt;/a&gt; so you can be sure they are not doing anything suspicious. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string AccessInfomationAnswer {
+            get {
+                return ResourceManager.GetString("AccessInfomationAnswer", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Q. Maybe they&apos;ll pull out your Discord account or credit card info? に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string AccessInfomationQuestion {
+            get {
+                return ResourceManager.GetString("AccessInfomationQuestion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Add Bot に類似しているローカライズされた文字列を検索します。
         /// </summary>
         internal static string AddBot {
             get {
                 return ResourceManager.GetString("AddBot", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Adding a Bot to the Discord Server に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string AddBotDiscord {
+            get {
+                return ResourceManager.GetString("AddBotDiscord", resourceCulture);
             }
         }
         
@@ -79,11 +124,56 @@ namespace StableDiffusionDiscordBot.Properties {
         }
         
         /// <summary>
+        ///   Charge に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string Charge {
+            get {
+                return ResourceManager.GetString("Charge", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   CustomerPortal に類似しているローカライズされた文字列を検索します。
         /// </summary>
         internal static string CustomerPortal {
             get {
                 return ResourceManager.GetString("CustomerPortal", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   0 to 10 sheets に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string FreeCount {
+            get {
+                return ResourceManager.GetString("FreeCount", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   US$0 (no usage fee) に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string FreePay {
+            get {
+                return ResourceManager.GetString("FreePay", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   The generated image is sent by reply from the bot. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string GeneratedImageFromBot {
+            get {
+                return ResourceManager.GetString("GeneratedImageFromBot", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   How to use this site に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string HowToUseThisSite {
+            get {
+                return ResourceManager.GetString("HowToUseThisSite", resourceCulture);
             }
         }
         
@@ -106,6 +196,15 @@ namespace StableDiffusionDiscordBot.Properties {
         }
         
         /// <summary>
+        ///   Login with your Discord account に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string LoginWithDiscord {
+            get {
+                return ResourceManager.GetString("LoginWithDiscord", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Logout に類似しているローカライズされた文字列を検索します。
         /// </summary>
         internal static string Logout {
@@ -120,6 +219,60 @@ namespace StableDiffusionDiscordBot.Properties {
         internal static string Model {
             get {
                 return ResourceManager.GetString("Model", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Number of images generated per month に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string NumberOfImages {
+            get {
+                return ResourceManager.GetString("NumberOfImages", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   A. Please contact the developer&apos;s &lt;a href=&quot;https://twitter.com/kawa0x0A&quot;&gt;Twitter account&lt;/a&gt; via reply or direct message. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string OtherProblemAnswer {
+            get {
+                return ResourceManager.GetString("OtherProblemAnswer", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Q. Other problems like this happened. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string OtherProblemQuestion {
+            get {
+                return ResourceManager.GetString("OtherProblemQuestion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Send a reply to the bot you want to use on Discord に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string SendDiscordToBot {
+            get {
+                return ResourceManager.GetString("SendDiscordToBot", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Sign up for a subscription に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string SignUpSubscription {
+            get {
+                return ResourceManager.GetString("SignUpSubscription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   You can add a bot to Discord that performs AI-based image generation. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string SiteDescription {
+            get {
+                return ResourceManager.GetString("SiteDescription", resourceCulture);
             }
         }
         
@@ -147,6 +300,51 @@ namespace StableDiffusionDiscordBot.Properties {
         internal static string Subscription {
             get {
                 return ResourceManager.GetString("Subscription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   11 sheets~. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string SubscriptionCount {
+            get {
+                return ResourceManager.GetString("SubscriptionCount", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   You do not have a subscription, please sign up for a subscription to start using Bot. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string SubscriptionDisableDescription {
+            get {
+                return ResourceManager.GetString("SubscriptionDisableDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   You have multiple subscriptions. Please check your subscription status in the Customer Portal as you may not have been billed correctly. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string SubscriptionDuplicationDescription {
+            get {
+                return ResourceManager.GetString("SubscriptionDuplicationDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Thank you for signing up for a subscription, you can add the Bot to your Discord server to use AI image generation, if you wish to terminate your use of the Bot, please terminate your subscription through the Customer Portal. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string SubscriptionEnableDescription {
+            get {
+                return ResourceManager.GetString("SubscriptionEnableDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   A fee of US$ 0.01 is charged for each sheet used. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string SubscriptionPay {
+            get {
+                return ResourceManager.GetString("SubscriptionPay", resourceCulture);
             }
         }
         
@@ -183,6 +381,51 @@ namespace StableDiffusionDiscordBot.Properties {
         internal static string WaifuDiffusionFeatures {
             get {
                 return ResourceManager.GetString("WaifuDiffusionFeatures", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Wait a while for the image to be generated に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string WaitWhileGenerated {
+            get {
+                return ResourceManager.GetString("WaitWhileGenerated", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   A. Please cancel your subscription through the Customer Portal button that appears after logging in on Discord. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string WantToCancelSubscriptionAnswer {
+            get {
+                return ResourceManager.GetString("WantToCancelSubscriptionAnswer", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Q. I want to cancel my subscription. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string WantToCancelSubscriptionQuestion {
+            get {
+                return ResourceManager.GetString("WantToCancelSubscriptionQuestion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   A. You will be billed for your subscription on a month-to-month basis upon signing up for a subscription. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string WhenBilledAnswer {
+            get {
+                return ResourceManager.GetString("WhenBilledAnswer", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Q. When will I be billed? に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string WhenBilliedQuestion {
+            get {
+                return ResourceManager.GetString("WhenBilliedQuestion", resourceCulture);
             }
         }
     }
