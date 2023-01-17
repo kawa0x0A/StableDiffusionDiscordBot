2d83f4a17ae2f6d75e7d21766f28371202bef926
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2023-01-17 10:03:38 +0900

    Update Success.cshtml.cs

--- Pages/Success.cshtml.cs
+++ Pages/Success.cshtml.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.OutputCaching;
 
 namespace StableDiffusionDiscordBot.Pages
 {
     public class SuccessModel : PageModel
     {
+        [OutputCache(NoStore = true, Duration = 0)]
         public IActionResult OnGet()
         {
-            return Redirect("/");
+            return LocalRedirect("/");
         }
     }
 }
